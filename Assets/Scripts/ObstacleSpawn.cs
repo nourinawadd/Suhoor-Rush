89b9261fafb114b9a82e72bdf3dc83c25ae393f2
nourinawadd <nourinawad@gmail.com>
2025-03-28 06:44:10 +0200

    Organize files

--- Assets/Scripts/ObstacleSpawn.cs
+++ Assets/Scripts/ObstacleSpawn.cs
@@ -1,32 +1,28 @@
-
 using UnityEngine;
 
-public class SweetsSpawner : MonoBehaviour
+public class ObstacleSpawn : MonoBehaviour
 {
-    public GameObject[] Prefab;
+    public GameObject obstacle;
     public float maxX;
     public float maxY;
     public float minX;
     public float minY;
     public float timeBetweenSpawn;
     private float spawnTime;
-
-   
-
-    // Update is called once per frame
     void Update()
     {
         if (Time.time > spawnTime)
         {
             Spawn();
             spawnTime = Time.time + timeBetweenSpawn;
-        }
+        }     
     }
+
     void Spawn()
     {
         float randomX = Random.Range(minX, maxX);
         float randomY = Random.Range(minY, maxY);
-        int Prefab_Index = Random.Range(0, Prefab.Length);
-        Instantiate(Prefab[Prefab_Index], transform.position + new Vector3(randomX, randomY, 0), Quaternion.identity);        
-    }    
+
+        Instantiate(obstacle, transform.position + new Vector3(randomX, randomY, 0), transform.rotation);
+    }
 }
